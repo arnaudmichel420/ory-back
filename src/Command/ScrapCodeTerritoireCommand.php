99142fc1e5c2e8d9706c74af67a1cff50bdd6cd7
arnaudmichel420 <arnaud.michel420@gmail.com>
2026-04-06 23:17:18 +0200

    feat : Ajout de la commande de scrap et du service pour scrap et créer les entités

---
+++ src/Command/ScrapCodeTerritoireCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Service\TerritoireServices;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'app:scrap-territoire')]
+final class ScrapCodeTerritoireCommand extends Command
+{
+    public function __construct(
+        private readonly TerritoireServices $territoireServices,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $result = $this->territoireServices->scrapTerritoire();
+
+        $output->writeln(sprintf('<info>%s</info>', $result['message']));
+        $output->writeln('');
+        $output->writeln('<comment>Territoires recuperes par type :</comment>');
+
+        foreach ($result['fetched'] as $type => $count) {
+            $output->writeln(sprintf(' - %s : %d', $type, $count));
+        }
+
+        $output->writeln('');
+        $output->writeln('<comment>Resultat import :</comment>');
+        $output->writeln(sprintf(' - recus : %d', $result['import']['total_received']));
+        $output->writeln(sprintf(' - crees : %d', $result['import']['created']));
+        $output->writeln(sprintf(' - mis a jour : %d', $result['import']['updated']));
+        $output->writeln(sprintf(' - invalides ignores : %d', $result['import']['skipped_invalid']));
+        $output->writeln(sprintf(' - parents relies : %d', $result['import']['parent_bound']));
+        $output->writeln(sprintf(' - parents introuvables : %d', $result['import']['parent_missing']));
+
+        return Command::SUCCESS;
+    }
+}
--- src/Repository/TerritoireRepository.php
+++ src/Repository/TerritoireRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Entity\Territoire;
+use App\Enum\TerritoireCodeTypeTerritoireEnum;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,5 +17,15 @@ class TerritoireRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Territoire::class);
+    }
+
+    public function findOneByTypeAndCode(
+        TerritoireCodeTypeTerritoireEnum $codeTypeTerritoire,
+        string $codeTerritoire,
+    ): ?Territoire {
+        return $this->findOneBy([
+            'codeTypeTerritoire' => $codeTypeTerritoire,
+            'codeTerritoire' => $codeTerritoire,
+        ]);
     }
 }
